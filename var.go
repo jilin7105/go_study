b3960c7c7de9c45e35d983dd2c5d9a0c273386c2
jilin7105 <443500251@qq.com>
2017-08-18 23:08:57 +0800

    变量定义    字符串必须用双引号

---
+++ var.go
@@ -0,0 +1,18 @@
+package main 
+
+
+var x,y int 
+var(
+	a int 
+	b bool
+)
+
+var c,d int = 1,2
+
+var e,f = 123 , "hello"
+func main(){
+	g,h :=123,"hello"
+	println(x,y,a,b,c,d,e,f,g,h)
+	v, b, n := "123" ,"1222" ,"2323"
+	println(v, b, n)
+}
